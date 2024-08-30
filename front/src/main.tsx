d86ebc270f331b250bc0b9bbe1650b513b920b81
MariamRatiani <mrati20@freeuni.edu.ge>
2024-08-30 21:26:36 +0400

    refactor: green homepage

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Playground from "./playground/Playground.tsx";
+import Homepage from "./homePage/Homepage.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Playground />
+        <Homepage />
     </React.StrictMode>,
 )
